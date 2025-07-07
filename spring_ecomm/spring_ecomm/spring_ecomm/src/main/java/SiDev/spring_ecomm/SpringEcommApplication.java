8fd1dba6347b53782fd2002f1980c0f322fd0e65
siddu017 <siddu22522@gmail.com>
2025-07-07 21:51:11 +0530

    added spring ecomm project

---
+++ spring_ecomm/spring_ecomm/spring_ecomm/src/main/java/SiDev/spring_ecomm/SpringEcommApplication.java
@@ -0,0 +1,13 @@
+package SiDev.spring_ecomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcommApplication.class, args);
+	}
+
+}
---
+++ spring_ecomm/spring_ecomm/spring_ecomm/src/test/java/SiDev/spring_ecomm/SpringEcommApplicationTests.java
@@ -0,0 +1,13 @@
+package SiDev.spring_ecomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
