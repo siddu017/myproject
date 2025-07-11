8ebb9ae778a8a6e4ab0476f1f831c755591cf573
siddu017 <siddu22522@gmail.com>
2025-07-11 21:44:24 +0530

    added entity details

--- spring_ecomm/src/main/java/SiDev/spring_ecomm/entity/productentity/ProductEntity.java
+++ spring_ecomm/src/main/java/SiDev/spring_ecomm/entity/productentity/ProductEntity.java
@@ -1,4 +1,66 @@
 package SiDev.spring_ecomm.entity.productentity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+    private String productCategory;
+    private String productName;
+    private double productPrice;
+    private String productDescription;
+    private String productImageUrl;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public String getProductImageUrl() {
+        return productImageUrl;
+    }
+
+    public void setProductImageUrl(String productImageUrl) {
+        this.productImageUrl = productImageUrl;
+    }
 }
--- spring_ecomm/src/main/java/SiDev/spring_ecomm/entity/userentity/UserEntity.java
+++ spring_ecomm/src/main/java/SiDev/spring_ecomm/entity/userentity/UserEntity.java
@@ -1,4 +1,57 @@
 package SiDev.spring_ecomm.entity.userentity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String userName;
+    private String userGmail;
+    private String userPhoneNo;
+    private String userPassword;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserGmail() {
+        return userGmail;
+    }
+
+    public void setUserGmail(String userGmail) {
+        this.userGmail = userGmail;
+    }
+
+    public String getUserPhoneNo() {
+        return userPhoneNo;
+    }
+
+    public void setUserPhoneNo(String userPhoneNo) {
+        this.userPhoneNo = userPhoneNo;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
 }
